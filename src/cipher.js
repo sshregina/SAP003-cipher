d59283152b3cdf52440f70b4878db8023d5d1d22
sshregina <sheila.regina.94@gmail.com>
2019-08-20 23:20:46 -0300

    Criação das funções de encode e decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,43 @@
+
+function encode(offset, string) {
+  const palavra = [];
+  for (let i=0;i<string.length;i++) {
+    const codigo = string[i].charCodeAt(0);
+    if (codigo >= 65 && codigo <= 90 ) {
+      const numerador = (codigo-65+offset)%26+65;
+      const letra = String.fromCharCode(numerador);
+      palavra.push(letra);
+    } else if (codigo >= 97 && codigo <= 122) {
+      const numerador = (codigo-97+offset)%26+97;
+      const letra = String.fromCharCode(numerador);
+      palavra.push(letra);
+    } else {
+      palavra.push(string[i]);
+    }
+  }
+  return palavra.join("");
+}
+
+function decode(offset, string) {
+  const palavra = [];
+  for (let i=0;i<string.length;i++) {
+    const codigo = string[i].charCodeAt(0);
+    if (codigo >= 65 && codigo <= 90 ) {
+      const numerador = (codigo-65-offset)%26+65;
+      const letra = String.fromCharCode(numerador);
+      palavra.push(letra);
+    } else if (codigo >= 97 && codigo <= 122) {
+      const numerador = (codigo-97-offset)%26+97;
+      const letra = String.fromCharCode(numerador);
+      palavra.push(letra);
+    } else {
+      palavra.push(string[i]);
+    }
+  }
+  return palavra.join("");
+}
+
 window.cipher = {
-  // ... 
+  encode,
+  decode
 };
